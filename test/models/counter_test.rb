d9a2f47b7e9ddb8c860b5db31a4f8375916adbf3
Paul Shippy <paul.shippy@strongmind.com>
2023-11-18 18:50:18 -0700

    Add spec for counter model

--- test/models/counter_test.rb
+++ test/models/counter_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require "minitest/autorun"
+require "minitest/spec"
 
-class CounterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe "Counter" do
+  describe "#up" do
+    it "adds 1 to the number" do
+      counter = Counter.new(number: 0)
+      counter.up
+      assert_equal 1, counter.number
+    end
+  end
+  describe "#down" do
+    it "subtracts 1 from the number" do
+      counter = Counter.new(number: 0)
+      counter.down
+      assert_equal -1, counter.number
+    end
+  end
+  describe "#reset" do
+    it "sets the number to 0" do
+      counter = Counter.new(number: 1)
+      counter.reset
+      assert_equal 0, counter.number
+    end
+  end
 end
