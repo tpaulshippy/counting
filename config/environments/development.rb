396e7f8c9da4fc1f45da4df5c7703e07da0c5164
Paul Shippy <paul.shippy@strongmind.com>
2023-11-25 15:18:34 -0700

    Add dotenv and host
    Co-authored-by: Judah Shippy <judahshippy@gmail.com>
    Co-authored-by: Micah Shippy <micahgshippy@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Read REMOTE_HOST from the environment
+  config.hosts << ENV["REMOTE_HOST"] if ENV["REMOTE_HOST"].present?
+
 end
