b24bebc47c2885dc1cf344f14982b591c5775259
Paul Shippy <paul.shippy@strongmind.com>
2023-11-25 17:31:46 -0700

    Remove basic auth
    Co-authored-by: Judah Shippy <judahshippy@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate
 
-   private
-
-   def authenticate
-     authenticate_or_request_with_http_basic do |username, password|
-       username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-     end
-   end
 end
