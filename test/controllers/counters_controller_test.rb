92829e52377d6b25af88097622d6ad443eee678e
Paul Shippy <paul.shippy@strongmind.com>
2023-12-09 20:51:17 -0700

    Fix controller tests after adding users

--- test/controllers/counters_controller_test.rb
+++ test/controllers/counters_controller_test.rb
@@ -2,7 +2,13 @@ require "test_helper"
 
 class CountersControllerTest < ActionDispatch::IntegrationTest
   setup do
+    user = users(:one)
+    user.password = user.encrypted_password
+    user.save
+    sign_in user
+
     @counter = counters(:one)
+    @counter.user_id = user.id
   end
 
   test "should get index" do
@@ -24,21 +30,25 @@ class CountersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show counter" do
+    @counter.save
     get counter_url(@counter)
     assert_response :success
   end
 
   test "should get edit" do
+    @counter.save
     get edit_counter_url(@counter)
     assert_response :success
   end
 
   test "should update counter" do
+    @counter.save
     patch counter_url(@counter), params: { counter: { name: @counter.name, number: @counter.number } }
     assert_redirected_to counter_url(@counter)
   end
 
   test "should destroy counter" do
+    @counter.save
     assert_difference("Counter.count", -1) do
       delete counter_url(@counter)
     end
@@ -47,6 +57,7 @@ class CountersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should add 1 to counter" do
+    @counter.save
     patch counter_url(@counter), params: { commit: "Up" }
     @counter.reload
     assert @counter.number == 1
@@ -54,6 +65,7 @@ class CountersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should subtract 1 from counter" do
+    @counter.save
     patch counter_url(@counter), params: { commit: "Down" }
     @counter.reload
     assert @counter.number == -1
@@ -61,6 +73,7 @@ class CountersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should reset counter" do
+    @counter.save
     patch counter_url(@counter), params: { commit: "Reset" }
     @counter.reload
     assert @counter.number == 0
