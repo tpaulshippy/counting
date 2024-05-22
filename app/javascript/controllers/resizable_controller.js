b12fef62d1a8c203f0b2d9a1b37038f04722367f
Paul Shippy <paul.shippy@strongmind.com>
2024-05-21 22:10:12 -0700

    add cookie to remember position

--- app/javascript/controllers/resizable_controller.js
+++ app/javascript/controllers/resizable_controller.js
@@ -6,7 +6,12 @@ export default class extends Controller {
   connect() {
     this.boundResize = this.resize.bind(this);
     this.boundStopResize = this.stopResize.bind(this);
-    
+
+    const savedWidth = this.getCookie('resizableWidth');
+    if (savedWidth) {
+      this.resizableTarget.style.width = savedWidth;
+    }
+
     this.handleTarget.addEventListener('mousedown', (e) => {
       e.preventDefault();
 
@@ -16,7 +21,9 @@ export default class extends Controller {
   }
 
   resize(e) {
-    this.resizableTarget.style.width = (e.clientX - this.resizableTarget.offsetLeft) + 'px';
+    const newWidth = (e.clientX - this.resizableTarget.offsetLeft) + 'px';
+    this.resizableTarget.style.width = newWidth;
+    this.setCookie('resizableWidth', newWidth);
   }
 
   stopResize() {
@@ -24,4 +31,21 @@ export default class extends Controller {
     document.removeEventListener('mouseup', this.boundStopResize);
   }
 
+  setCookie(name, value) {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + 10); // Set the date 10 years in the future
+    const expires = "; expires=" + date.toUTCString();
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
 }
